b4f3ccdcb248e3e1fa6464e8d5287d898925f876
haruyama-makoto <makoto.haruyama@dena.com>
2014-07-04 11:26:41 +0900

    Change delimiter space to tab

--- lib/unicorn/log_error_one_liner.rb
+++ lib/unicorn/log_error_one_liner.rb
@@ -4,7 +4,7 @@ module Unicorn
   def self.log_error(logger, prefix, exc)
     message = exc.message
     message = message.dump if /[[:cntrl:]]/ =~ message
-    backtrace_str = exc.backtrace.join(' ')
+    backtrace_str = exc.backtrace.join("\t")
     logger.error "#{prefix}: #{message} (#{exc.class}) #{backtrace_str}"
   end
 end
